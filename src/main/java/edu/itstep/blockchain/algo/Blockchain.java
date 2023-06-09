c2839c1c4c4bc89030a2de4f36cf4edd3a59928b
User <java.user.tester@gmail.com>
2023-06-09 19:55:24 +0300

    Refractoring bitcoin

---
+++ src/main/java/edu/itstep/blockchain/algo/Blockchain.java
@@ -0,0 +1,43 @@
+package edu.itstep.blockchain.algo;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import edu.itstep.blockchain.transactions.TransactionOutput;
+
+public class Blockchain {
+
+	   // this is the public ledger - everyone can access
+	   // all the blocks (previous transactions) on the blockchain
+	   public static List<Block> blockChain;
+	   // we store every unspent transactions
+	   public static Map<String, TransactionOutput> UTXOs; 
+	   
+	   public Blockchain() {
+	      Blockchain.UTXOs = new HashMap<String,TransactionOutput>();
+	      Blockchain.blockChain = new ArrayList<>();
+	   }
+	   
+	   public void addBlock(Block block) {
+	      Blockchain.blockChain.add(block);
+	   }
+	   
+	   public int size() {
+	      return Blockchain.blockChain.size();
+	   }
+	   
+	   @Override
+	   public String toString() {
+
+	      String blockChain = "";
+	      
+	      for(Block block : Blockchain.blockChain)
+	         blockChain+=block.toString()+"\n";
+	      
+	      return blockChain;
+	   }
+	}
+
+
