6de82479942e10a8dcf3fb1ee5fd2158856d3b39
User <java.user.tester@gmail.com>
2023-06-09 21:11:28 +0300

    Entity and Reposotiry added

--- src/main/java/edu/itstep/blockchain/repository/TransactionPersistentRepository.java
+++ src/main/java/edu/itstep/blockchain/repository/TransactionPersistentRepository.java
@@ -1,4 +1,4 @@
-package edu.itstep.blockchain.reposotiry;
+package edu.itstep.blockchain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
